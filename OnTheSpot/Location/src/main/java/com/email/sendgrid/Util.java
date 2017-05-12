f380d20d60232c7862c4e70ba7cd07c863bbbc76
suhel4444 <suhel.mehta@sjsu.edu>
2017-05-12 15:36:17 -0700

    Updating credentials for sendgrid

--- OnTheSpot/Location/src/main/java/com/email/sendgrid/Util.java
+++ OnTheSpot/Location/src/main/java/com/email/sendgrid/Util.java
@@ -1,8 +1,8 @@
 package com.email.sendgrid;
 
 public class Util {
-    public static final String username = "suhel123" ;
-    public static final String password = "Sjsu2018" ;
+    public static final String username = "username" ;
+    public static final String password = "password" ;
 
     public String getUsername() {
         return username;
